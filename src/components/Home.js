bb4f5964c7768baed48651d0ae4d6d932fcdb6d9
animesh28 <animesh.raj.om@gmail.com>
2022-02-17 20:54:06 +0530

    section modular functionality

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,60 @@ import Section from './Section'
 function Home() {
   return (
     <Container>
-        <Section/>
+        <Section
+          title="Model S"
+          description="Order online for touchless delivery!"
+          backgroundImg="model-s.jpg"
+          leftBtnText="Custom Order"
+          rightBtnText="Existing Inventory"
+        />
+
+        <Section
+          title="Model Y"
+          description="Order online for touchless delivery!"
+          backgroundImg="model-y.jpg"
+          leftBtnText="Custom Order"
+          rightBtnText="Existing Inventory"
+        />
+
+        <Section
+          title="Model 3"
+          description="Order online for touchless delivery!"
+          backgroundImg="model-3.jpg"
+          leftBtnText="Custom Order"
+          rightBtnText="Existing Inventory"
+        />
+
+        <Section
+          title="Model X"
+          description="Order online for touchless delivery!"
+          backgroundImg="model-x.jpg"
+          leftBtnText="Custom Order"
+          rightBtnText="Existing Inventory"
+        />
+
+        <Section
+          title="Lowest Solar Panels in America"
+          description="Cashback Guarantee!"
+          backgroundImg="solar-panel.jpg"
+          leftBtnText="Order Now"
+          rightBtnText="Learn More"
+        />
+
+        <Section
+          title="Solar for New Roofs"
+          description="Solar Roofs Costs Less Than a New Roof Plus Solar Panels!"
+          backgroundImg="solar-roof.jpg"
+          leftBtnText="Order Now"
+          rightBtnText="Learn More"
+        />
+
+        <Section
+          title="Accessories"
+          description="All you need for you!"
+          backgroundImg="accessories.jpg"
+          leftBtnText="Order Now"
+        />
     </Container>
   )
 }
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,23 +1,25 @@
 import React from 'react'
 import styled from 'styled-components'
 
-function Section() {
+function Section({title, description, backgroundImg, leftBtnText, rightBtnText}) {
   return (
-    <Wrap>
+    <Wrap bgImg={backgroundImg}>
         <ItemText>
-            <h1>Model S</h1>
-            <p>Order online for touchless delivery!</p>
+            <h1>{title}</h1>
+            <p>{description}</p>
         </ItemText>
 
         <Buttons>
             <ButtonGroup>
                 <LeftButton>
-                    Custom Order
+                    {leftBtnText}
                 </LeftButton>
-
-                <RightButton>
-                    Existing Inventory
-                </RightButton>
+                {   rightBtnText &&
+                    <RightButton>
+                        {rightBtnText}
+                    </RightButton>
+                }
+                
             </ButtonGroup>
 
             <DownArrow src="./images/down-arrow.svg"/>
@@ -31,7 +33,7 @@ export default Section
 const Wrap = styled.div`
     width: 100vw;
     height: 100vh;
-    background: url('./images/model-s.jpg');
+    background: ${props => `url(./images/${props.bgImg})`};
     background-position: center;
     background-repeat: no-repeat;
     background-size: cover;
@@ -55,6 +57,10 @@ const Buttons = styled.div`
 const ButtonGroup = styled.div`
     display: flex;
     margin-bottom: 30px;
+
+    @media screen and (max-width: 786px) {
+        flex-direction: column;
+    }
 `
 
 const LeftButton = styled.div`
